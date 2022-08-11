ba779e530e94e49daf434a2e5a75ef3d5d85ae43
@lhhuyIT <huy.le@idts.com.vn>
2022-08-11 23:19:18 +0700

    [intergrate_di] intergrate injectable and build_runner

--- lib/cubit/movie_details/movie_details_cubit.dart
+++ lib/cubit/movie_details/movie_details_cubit.dart
@@ -1,9 +1,11 @@
 import 'package:bloc/bloc.dart';
+import 'package:injectable/injectable.dart';
 import 'package:movies_app/data/models/movies/movie_details.dart';
 import 'package:movies_app/data/repositories/movie_repository.dart';
 
 part 'movie_details_state.dart';
 
+@lazySingleton
 class MovieDetailsCubit extends Cubit<MovieDetailsState> {
   final MovieRepository movieRepository;
 
---
+++ lib/injection.config.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+
+import 'cubit/movie_details/movie_details_cubit.dart' as _i5;
+import 'cubit/movies/movies_cubit.dart' as _i4;
+import 'data/repositories/movie_repository.dart'
+    as _i3; // ignore_for_file: unnecessary_lambdas
+
+// ignore_for_file: lines_longer_than_80_chars
+/// initializes the registration of provided dependencies inside of [GetIt]
+_i1.GetIt $initGetIt(_i1.GetIt get,
+    {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
+  final gh = _i2.GetItHelper(get, environment, environmentFilter);
+  gh.singleton<_i3.MovieRepository>(_i3.MovieRepository());
+  gh.singleton<_i4.MoviesCubit>(_i4.MoviesCubit());
+  gh.lazySingleton<_i5.MovieDetailsCubit>(
+      () => _i5.MovieDetailsCubit(movieRepository: get<_i3.MovieRepository>()));
+  return get;
+}
